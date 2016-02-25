a149c8c1512a32486e4077ba14862c67b36c7c87
jryan <joseph.ryan@laterooms.com>
2016-02-25 11:32:58 +0000

    listing results out of es

--- static/javascripts/search.js
+++ static/javascripts/search.js
@@ -1,10 +1,37 @@
 window.onload = function(){
+    var resultsBox = $('#results');
+    var searchBox = $('#search-box');
+
+    function parse(results){
+        resultsBox.html('');
+        console.log(results.hits.hits.length);
+        results.hits.hits.forEach(createResultEntry);
+    }
+
+    function createResultEntry(result) {
+        var source = result._source;
+        var entry = $('<div class="result">'+
+            '<span>' + source.hostName + '</span>' +
+            '</div>');
+
+        resultsBox.append(entry)
+    }
+
     function createSearch() {
-        var results = $('#results');
-        var searchBox = $('#search-box');
         function doSearch() {
+            resultsBox.html('Searching now');
             var text = searchBox[0].value;
-            results.html('You searched for ' + text);
+            if(text === "") {
+                resultsBox.html('');
+                return;
+            }
+            $.ajax({
+                url:"http://logs.laterooms.com:9200/servers/_search?size=100&q=hostName:" + text + "*"
+            })
+            .success(parse)
+            .error(function (err) {
+                resultsBox.html('encountered an error: ' + err)
+            });
         }
 
         function bindSearchEvents(){
