00b08be5ed58b19edbc79729e9b2fc93b35b4bc6
jryan <joseph.ryan@laterooms.com>
2016-03-14 11:35:31 +0000

    added guard checks

--- static/javascripts/esQuery.js
+++ static/javascripts/esQuery.js
@@ -6,11 +6,16 @@ function createElasticsearchInterface(){
                     url:'http://logs.laterooms.com:9200/servers/_search?size=100&q=' + query
                 })
                 .success(function (serverResults) {
+                    if(!serverResults.hits.hits[0]) {
+                        return resolve(serverResults);
+                    }
                     return $.ajax({
                         url: 'http://logs.laterooms.com:9200/loadbalancer/pools/_search?q=basic.nodes_table.node:"' + serverResults.hits.hits[0]._source.primaryIPAddress + ':*"'
                     })
                     .success(function (poolResults){
-                        serverResults.hits.hits[0]._source.poolName = poolResults.hits.hits[0]._id;
+                        if(poolResults.hits.hits[0]){
+                            serverResults.hits.hits[0]._source.poolName = poolResults.hits.hits[0]._id;
+                        }
                         resolve(serverResults);
                     })
                 })
