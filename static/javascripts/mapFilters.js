583b602b8d900f15b4167b888264a2577d8897af
jryan <joseph.ryan@laterooms.com>
2016-03-15 14:23:43 +0000

    andy schooled me in async

--- static/javascripts/mapFilters.js
+++ static/javascripts/mapFilters.js
@@ -3,6 +3,8 @@ function createFilterMapper(){
     return {
         map: function (hits) {
             if(!hits) return {};
+
+            return _.groupBy(hits, '_source.systemStatus');
         }
     }
 }
--- static/javascripts/renderer.js
+++ static/javascripts/renderer.js
@@ -7,20 +7,12 @@ function createRenderer(resultsMapper, filterMapper, fileLoader) {
             var filters = filterMapper.map(results.hits.hits);
             var renderedResults;
 
-
-            return fileLoader.load('/static/javascripts/templates/result.html')
-                .then(function (template) {
-                    return Mustache.render(template, parsed);
-                })
-                .then(function (moustacheRender) {
-                    renderedResults = moustacheRender;
-                    return fileLoader.load('/static/javascripts/templates/filters.html');
-                })
-                .then(function (filterTemplate) {
+            return fileLoader.all(['/static/javascripts/templates/results.html', '/static/javascripts/templates/filters.html'])
+                .then(function (templates) {
                     return {
-                        results: renderedResults,
-                        filters: Mustache.render(filterTemplate, filters)
-                    };
+                        results: Mustache.render(templates[0], parsed),
+                        filters: Mustache.render(templates[1], filters),
+                    }
                 });
         }
     }
